2d16272d4731a53db0de4f1b9de7b8f7ff38d428
nsc-wuhan <hwu@eppei.com>
2021-03-09 00:30:57 +0800

    update

--- src/router.js
+++ src/router.js
@@ -5,12 +5,12 @@ import About from "./components/about";
 import MyPage from "./pages";
 // console.log("window.__POWERED_BY_QIANKUN__", window.__POWERED_BY_QIANKUN__);
 const BasicRoute = () => (
-  <HashRouter basename="/app1">
+  <BrowserRouter basename="/app1">
     <Switch>
       <Route exact path="/" component={MyPage} />
       <Route exact path="/about" component={() => <About />} />
     </Switch>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default BasicRoute;
